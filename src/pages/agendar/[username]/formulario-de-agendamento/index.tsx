7d6f79d92e2eaa363651856ca6523678631f74f6
Gustavo Buziki <gustavo.buziki@paipe.co>
2023-10-19 10:48:55 -0300

    feat: schedule and confirm appointment

---
+++ src/pages/agendar/[username]/formulario-de-agendamento/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import { ConfirmStep } from './confirm-step'
+import { CalendarStep } from './calendar-step'
+
+export function ScheduleForm() {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
+}
---
+++ src/pages/agendar/[username]/index.page.tsx
@@ -0,0 +1,67 @@
+import { NextSeo } from 'next-seo'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+
+import { prisma } from '~/lib/prisma'
+import { Container, UserHeader } from './styles'
+import { ScheduleForm } from './formulario-de-agendamento'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    avatarUrl: string
+    bio: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <>
+      <NextSeo title={`Agendar com ${user.name} | Ignite Call`} />
+
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.name}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+
+        <ScheduleForm />
+      </Container>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+
+  if (!user) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        avatarUrl: user.avatar_url,
+        bio: user.bio
+      }
+    },
+    revalidate: 60 * 60 * 24 //1 day
+  }
+}
